58074bf5b82a3889d3a7fa238e720d15e97ec0eb
jalpate151 <jalpate151@SDS-L-H982Y93>
2024-09-27 10:34:05 -0400

    Add project files.

---
+++ Freakbob's rizzarea/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Freakbob_s_rizzarea
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewOrder = new System.Windows.Forms.Button();
+            this.PrintRecepit = new System.Windows.Forms.Button();
+            this.ChangeLabel = new System.Windows.Forms.Label();
+            this.Change = new System.Windows.Forms.Label();
+            this.CalulateButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Tendred = new System.Windows.Forms.Label();
+            this.TotalText = new System.Windows.Forms.Label();
+            this.TaxText = new System.Windows.Forms.Label();
+            this.SubTotalText = new System.Windows.Forms.Label();
+            this.TotalLabel = new System.Windows.Forms.Label();
+            this.SubTotalLabel = new System.Windows.Forms.Label();
+            this.TaxLabel = new System.Windows.Forms.Label();
+            this.CalculateTotals = new System.Windows.Forms.Button();
+            this.SkibidiSlicers = new System.Windows.Forms.Label();
+            this.NettspendNuggets = new System.Windows.Forms.Label();
+            this.GeekedBajaBlast = new System.Windows.Forms.Label();
+            this.errorOutput = new System.Windows.Forms.Label();
+            this.NettspendNuggetsTextBox = new System.Windows.Forms.TextBox();
+            this.GeekedBajaBlastTexBox = new System.Windows.Forms.TextBox();
+            this.SkibidiSlicersTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NewOrder
+            // 
+            this.NewOrder.BackColor = System.Drawing.Color.BlueViolet;
+            this.NewOrder.ForeColor = System.Drawing.Color.Yellow;
+            this.NewOrder.Location = new System.Drawing.Point(497, 464);
+            this.NewOrder.Name = "NewOrder";
+            this.NewOrder.Size = new System.Drawing.Size(238, 34);
+            this.NewOrder.TabIndex = 41;
+            this.NewOrder.Text = "New Order";
+            this.NewOrder.UseVisualStyleBackColor = false;
+            // 
+            // PrintRecepit
+            // 
+            this.PrintRecepit.Location = new System.Drawing.Point(75, 467);
+            this.PrintRecepit.Name = "PrintRecepit";
+            this.PrintRecepit.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.PrintRecepit.Size = new System.Drawing.Size(151, 23);
+            this.PrintRecepit.TabIndex = 40;
+            this.PrintRecepit.Text = "Print Recepit";
+            this.PrintRecepit.UseVisualStyleBackColor = true;
+            // 
+            // ChangeLabel
+            // 
+            this.ChangeLabel.AutoSize = true;
+            this.ChangeLabel.Location = new System.Drawing.Point(154, 417);
+            this.ChangeLabel.Name = "ChangeLabel";
+            this.ChangeLabel.Size = new System.Drawing.Size(0, 16);
+            this.ChangeLabel.TabIndex = 39;
+            // 
+            // Change
+            // 
+            this.Change.AutoSize = true;
+            this.Change.BackColor = System.Drawing.Color.Purple;
+            this.Change.ForeColor = System.Drawing.Color.Yellow;
+            this.Change.Location = new System.Drawing.Point(75, 417);
+            this.Change.Name = "Change";
+            this.Change.Size = new System.Drawing.Size(54, 16);
+            this.Change.TabIndex = 38;
+            this.Change.Text = "Change";
+            // 
+            // CalulateButton
+            // 
+            this.CalulateButton.Location = new System.Drawing.Point(106, 377);
+            this.CalulateButton.Name = "CalulateButton";
+            this.CalulateButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.CalulateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalulateButton.TabIndex = 37;
+            this.CalulateButton.Text = "Calulate";
+            this.CalulateButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 349);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 22);
+            this.textBox1.TabIndex = 36;
+            // 
+            // Tendred
+            // 
+            this.Tendred.AutoSize = true;
+            this.Tendred.BackColor = System.Drawing.Color.Purple;
+            this.Tendred.ForeColor = System.Drawing.Color.Yellow;
+            this.Tendred.Location = new System.Drawing.Point(75, 355);
+            this.Tendred.Name = "Tendred";
+            this.Tendred.Size = new System.Drawing.Size(59, 16);
+            this.Tendred.TabIndex = 35;
+            this.Tendred.Text = "Tendred";
+            // 
+            // TotalText
+            // 
+            this.TotalText.AutoSize = true;
+            this.TotalText.Location = new System.Drawing.Point(154, 324);
+            this.TotalText.Name = "TotalText";
+            this.TotalText.Size = new System.Drawing.Size(0, 16);
+            this.TotalText.TabIndex = 34;
+            // 
+            // TaxText
+            // 
+            this.TaxText.AutoSize = true;
+            this.TaxText.Location = new System.Drawing.Point(154, 294);
+            this.TaxText.Name = "TaxText";
+            this.TaxText.Size = new System.Drawing.Size(0, 16);
+            this.TaxText.TabIndex = 33;
+            // 
+            // SubTotalText
+            // 
+            this.SubTotalText.AutoSize = true;
+            this.SubTotalText.Location = new System.Drawing.Point(154, 266);
+            this.SubTotalText.Name = "SubTotalText";
+            this.SubTotalText.Size = new System.Drawing.Size(0, 16);
+            this.SubTotalText.TabIndex = 32;
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.AutoSize = true;
+            this.TotalLabel.BackColor = System.Drawing.Color.Purple;
+            this.TotalLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.TotalLabel.Location = new System.Drawing.Point(75, 324);
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(38, 16);
+            this.TotalLabel.TabIndex = 31;
+            this.TotalLabel.Text = "Total";
+            // 
+            // SubTotalLabel
+            // 
+            this.SubTotalLabel.AutoSize = true;
+            this.SubTotalLabel.BackColor = System.Drawing.Color.Purple;
+            this.SubTotalLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.SubTotalLabel.Location = new System.Drawing.Point(72, 266);
+            this.SubTotalLabel.Name = "SubTotalLabel";
+            this.SubTotalLabel.Size = new System.Drawing.Size(65, 16);
+            this.SubTotalLabel.TabIndex = 30;
+            this.SubTotalLabel.Text = "Sub Total";
+            // 
+            // TaxLabel
+            // 
+            this.TaxLabel.AutoSize = true;
+            this.TaxLabel.BackColor = System.Drawing.Color.Purple;
+            this.TaxLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.TaxLabel.Location = new System.Drawing.Point(75, 294);
+            this.TaxLabel.Name = "TaxLabel";
+            this.TaxLabel.Size = new System.Drawing.Size(30, 16);
+            this.TaxLabel.TabIndex = 29;
+            this.TaxLabel.Text = "Tax";
+            // 
+            // CalculateTotals
+            // 
+            this.CalculateTotals.BackColor = System.Drawing.Color.BlueViolet;
+            this.CalculateTotals.ForeColor = System.Drawing.Color.Yellow;
+            this.CalculateTotals.Location = new System.Drawing.Point(119, 203);
+            this.CalculateTotals.Name = "CalculateTotals";
+            this.CalculateTotals.Size = new System.Drawing.Size(197, 46);
+            this.CalculateTotals.TabIndex = 28;
+            this.CalculateTotals.Text = "Calculate Totals";
+            this.CalculateTotals.UseVisualStyleBackColor = false;
+            this.CalculateTotals.Click += new System.EventHandler(this.CalculateTotals_Click);
+            // 
+            // SkibidiSlicers
+            // 
+            this.SkibidiSlicers.AutoSize = true;
+            this.SkibidiSlicers.BackColor = System.Drawing.Color.Purple;
+            this.SkibidiSlicers.ForeColor = System.Drawing.Color.Yellow;
+            this.SkibidiSlicers.Location = new System.Drawing.Point(75, 71);
+            this.SkibidiSlicers.Name = "SkibidiSlicers";
+            this.SkibidiSlicers.Size = new System.Drawing.Size(92, 16);
+            this.SkibidiSlicers.TabIndex = 27;
+            this.SkibidiSlicers.Text = "Skibidi Slicers";
+            // 
+            // NettspendNuggets
+            // 
+            this.NettspendNuggets.AutoSize = true;
+            this.NettspendNuggets.ForeColor = System.Drawing.Color.Yellow;
+            this.NettspendNuggets.Location = new System.Drawing.Point(75, 157);
+            this.NettspendNuggets.Name = "NettspendNuggets";
+            this.NettspendNuggets.Size = new System.Drawing.Size(123, 16);
+            this.NettspendNuggets.TabIndex = 26;
+            this.NettspendNuggets.Text = "Nettspend Nuggets";
+            // 
+            // GeekedBajaBlast
+            // 
+            this.GeekedBajaBlast.AutoSize = true;
+            this.GeekedBajaBlast.ForeColor = System.Drawing.Color.Yellow;
+            this.GeekedBajaBlast.Location = new System.Drawing.Point(72, 106);
+            this.GeekedBajaBlast.Name = "GeekedBajaBlast";
+            this.GeekedBajaBlast.Size = new System.Drawing.Size(120, 16);
+            this.GeekedBajaBlast.TabIndex = 25;
+            this.GeekedBajaBlast.Text = "Geeked Baja Blast";
+            // 
+            // errorOutput
+            // 
+            this.errorOutput.AutoSize = true;
+            this.errorOutput.Font = new System.Drawing.Font("Modern No. 20", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorOutput.ForeColor = System.Drawing.Color.Yellow;
+            this.errorOutput.Location = new System.Drawing.Point(99, 9);
+            this.errorOutput.Name = "errorOutput";
+            this.errorOutput.Size = new System.Drawing.Size(331, 38);
+            this.errorOutput.TabIndex = 24;
+            this.errorOutput.Text = "FreakBob\'s Rizzarea";
+            // 
+            // NettspendNuggetsTextBox
+            // 
+            this.NettspendNuggetsTextBox.Location = new System.Drawing.Point(226, 157);
+            this.NettspendNuggetsTextBox.Name = "NettspendNuggetsTextBox";
+            this.NettspendNuggetsTextBox.Size = new System.Drawing.Size(100, 22);
+            this.NettspendNuggetsTextBox.TabIndex = 23;
+            // 
+            // GeekedBajaBlastTexBox
+            // 
+            this.GeekedBajaBlastTexBox.Location = new System.Drawing.Point(226, 106);
+            this.GeekedBajaBlastTexBox.Name = "GeekedBajaBlastTexBox";
+            this.GeekedBajaBlastTexBox.Size = new System.Drawing.Size(100, 22);
+            this.GeekedBajaBlastTexBox.TabIndex = 22;
+            // 
+            // SkibidiSlicersTextBox
+            // 
+            this.SkibidiSlicersTextBox.Location = new System.Drawing.Point(226, 71);
+            this.SkibidiSlicersTextBox.Name = "SkibidiSlicersTextBox";
+            this.SkibidiSlicersTextBox.Size = new System.Drawing.Size(100, 22);
+            this.SkibidiSlicersTextBox.TabIndex = 21;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(800, 506);
+            this.Controls.Add(this.NewOrder);
+            this.Controls.Add(this.PrintRecepit);
+            this.Controls.Add(this.ChangeLabel);
+            this.Controls.Add(this.Change);
+            this.Controls.Add(this.CalulateButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Tendred);
+            this.Controls.Add(this.TotalText);
+            this.Controls.Add(this.TaxText);
+            this.Controls.Add(this.SubTotalText);
+            this.Controls.Add(this.TotalLabel);
+            this.Controls.Add(this.SubTotalLabel);
+            this.Controls.Add(this.TaxLabel);
+            this.Controls.Add(this.CalculateTotals);
+            this.Controls.Add(this.SkibidiSlicers);
+            this.Controls.Add(this.NettspendNuggets);
+            this.Controls.Add(this.GeekedBajaBlast);
+            this.Controls.Add(this.errorOutput);
+            this.Controls.Add(this.NettspendNuggetsTextBox);
+            this.Controls.Add(this.GeekedBajaBlastTexBox);
+            this.Controls.Add(this.SkibidiSlicersTextBox);
+            this.Name = "form1";
+            this.Text = "Freakbob\'s Rizzarea";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NewOrder;
+        private System.Windows.Forms.Button PrintRecepit;
+        private System.Windows.Forms.Label ChangeLabel;
+        private System.Windows.Forms.Label Change;
+        private System.Windows.Forms.Button CalulateButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Tendred;
+        private System.Windows.Forms.Label TotalText;
+        private System.Windows.Forms.Label TaxText;
+        private System.Windows.Forms.Label SubTotalText;
+        private System.Windows.Forms.Label TotalLabel;
+        private System.Windows.Forms.Label SubTotalLabel;
+        private System.Windows.Forms.Label TaxLabel;
+        private System.Windows.Forms.Button CalculateTotals;
+        private System.Windows.Forms.Label SkibidiSlicers;
+        private System.Windows.Forms.Label NettspendNuggets;
+        private System.Windows.Forms.Label GeekedBajaBlast;
+        private System.Windows.Forms.Label errorOutput;
+        private System.Windows.Forms.TextBox NettspendNuggetsTextBox;
+        private System.Windows.Forms.TextBox GeekedBajaBlastTexBox;
+        private System.Windows.Forms.TextBox SkibidiSlicersTextBox;
+    }
+}
+
---
+++ Freakbob's rizzarea/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Freakbob_s_rizzarea
+{
+    public partial class form1 : Form
+    {
+        double skibidiSlicersPrice;
+        double geekedBajaBlastPrice;
+        double nettspendNuggetsPrice;
+        double skibidiSlicersNumber;
+        double geekedBajaBlastNumber;
+        double nettspendNuggetsNumber;
+        double subtotal;
+        double total;
+        double tendred;
+        double change;
+        double tax;
+        double skibidiSlicersVari;
+        double geekedBajaBlastVari;
+        double nettspendNuggetsVari;
+
+        
+
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateTotals_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                errorOutput.Text = "FreakBob's Rizzarea";
+
+                skibidiSlicersPrice = 2.55;
+                geekedBajaBlastPrice = 4.50;
+                nettspendNuggetsPrice = 3.40;
+                skibidiSlicersNumber = Convert.ToDouble(SkibidiSlicersTextBox.Text);
+                geekedBajaBlastNumber = Convert.ToDouble(GeekedBajaBlastTexBox.Text);
+                nettspendNuggetsNumber = Convert.ToDouble(NettspendNuggetsTextBox.Text);
+                skibidiSlicersVari = skibidiSlicersPrice * skibidiSlicersNumber;
+                geekedBajaBlastVari =  geekedBajaBlastPrice * geekedBajaBlastNumber;
+                nettspendNuggetsVari =  nettspendNuggetsPrice * nettspendNuggetsNumber;
+
+                subtotal = skibidiSlicersVari + geekedBajaBlastVari + nettspendNuggetsVari;
+                tax = subtotal * .13;
+                total = subtotal + tax;
+
+                SubTotalText.Text = $"{subtotal.ToString("C")}";
+
+                TaxText.Text = $"{tax.ToString("C")}";
+
+                TaxText.Text = $"{tax.ToString("C")}";
+
+
+
+
+
+
+
+            }
+
+            catch
+            {
+
+            }
+        }
+    }
+}
